5060533166bede9e6368e11b5648b65f69976dba
ファム・クアン・ハー <ha.pq194546@sis.hust.edu.vn>
2023-06-03 12:03:53 +0000

    homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,51 +1,66 @@
 import classNames from 'classnames';
 import * as React from 'react';
 import styles from './Home.module.scss';
+import { Link } from 'react-router-dom';
 
 interface HomePageProps {}
 
 const HomePage: React.FunctionComponent<HomePageProps> = (props) => {
   return (
     <>
-      <div className={classNames(styles.hero, 'gap-8 text-white')}>
-        <h1 className="text-5xl">ようこそ、バナナ先生へ</h1>
-        <p className="text-xl">ベトナムで一番家庭教師を探すサイト</p>
+      <div className={classNames(styles.hero, 'gap-8')}>
+        <div className={classNames(styles.bluebox)}></div>
+        <h1 className={classNames(styles.slogan, 'text-8xl')}>ようこそ、<span className='text-red-500'>バナナ先生</span>へ</h1>
+        <p className={classNames(styles.subslogan, 'text-5xl')}>ベトナムで<span className='text-yellow-500'>一番</span>家庭教師を探すサイト</p>
       </div>
 
       <div className="container">
         <section className="my-14">
-          <h2 className="mb-10 text-3xl">今月の先生</h2>
+          <h2 className={classNames(styles.title,'mb-10 text-5xl')}>今月の先生</h2>
           <div className="grid grid-cols-3 gap-4">
-            {[...Array(6)].map(() => (
+            {[...Array(9)].map(() => (
               <div className="flex flex-col gap-3">
-                <div className="h-40 w-full">
+                <div className="h-60 w-full">
                   <img
-                    src="https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+                    src="https://iowacapitaldispatch.com/wp-content/uploads/2023/03/math-teacher-at-blackboard-1024x680.jpg"
                     alt="Teacher"
                     className="h-full w-full rounded-lg object-cover"
                   />
                 </div>
-                <div>先生の名前</div>
+                <div className="font-bold text-xl">先生の名前</div>
+                <div className="font-extralight">先生の情報</div>
               </div>
             ))}
           </div>
         </section>
 
         <section className="my-14">
-          <h2 className="mb-10 text-3xl">新しい先生</h2>
+          <h2 className={classNames(styles.title,'mb-10 text-5xl')}>新しい先生</h2>
           <div className="grid grid-cols-3 gap-4">
-            {[...Array(6)].map(() => (
+            {[...Array(9)].map(() => (
               <div className="flex flex-col gap-3">
-                <div className="h-40 w-full">
+                <div className="h-60 w-full">
                   <img
-                    src="https://images.unsplash.com/photo-1544717305-2782549b5136?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+                    src="https://t4.ftcdn.net/jpg/01/13/31/65/360_F_113316547_q9wiDxadvidz5UvKITGbJMvzqrDw45Kl.jpg"
                     alt="Teacher"
                     className="h-full w-full rounded-lg object-cover"
                   />
                 </div>
-                <div>先生の名前</div>
+                <div className="font-bold text-xl">先生の名前</div>
+                <div className="font-extralight">先生の情報</div>
               </div>
             ))}
+          </div>
+        </section>
+      </div>
+
+      <div className="container">
+        <section className='flex flex-col items-center justify-center'>
+          <h2 className='mb-10 text-4xl font-bold'>似合う先生を探そう</h2>
+          <div className="flex flex-row gap-3">
+            <Link to="/auth/login" className="rounded-lg bg-teal-500 px-20 py-3 text-xl text-white">
+              検索
+            </Link>
           </div>
         </section>
       </div>
