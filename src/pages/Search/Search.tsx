6b02794719f1a9be15ba43a7fe415dfd190df7d1
ファム・クアン・ハー <ha.pq194546@sis.hust.edu.vn>
2023-06-06 08:59:16 +0000

    connect API

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,15 +1,75 @@
-import * as React from 'react';
+import React, {useEffect, useState} from "react";
 import classNames from 'classnames';
 import styles from './Search.module.scss';
+import {searchTeacher} from "../../service/teacher";
+import { Pagination } from 'antd';
+import type { PaginationProps } from 'antd';
 
 interface SearchPageProps {}
 
+interface DataType {
+  page: number|undefined,
+  gender: number|undefined,
+  name: string|undefined,
+  experience_year: number|undefined,
+  fee: number|undefined,
+}
+
 const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
+  const [dataSource, setDataSource] = useState<[]>();
+  const [total, setTotal] = useState(0);
+  const [current, setCurrent] = useState(1);
+
+  const [name, setName] = useState<string|undefined>(undefined);
+  const [gender, setGender] = useState<number|undefined>(undefined);
+  const [fee, setFee] = useState<number|undefined>(undefined);
+  const [experience, setExperience] = useState<number|undefined>(undefined);
+  const [rate, setRate] = useState<number|undefined>(undefined);
+
+  const [search, setSearch] = useState<DataType>({
+    page: undefined,
+    gender: undefined,
+    name: undefined,
+    experience_year: undefined,
+    fee: undefined,
+  });
+
+  useEffect(() => {
+    searchTeacher(search).then((val) => {
+      setDataSource(val.data.data);
+      setTotal(val.data.total);
+      setCurrent(1);
+      // console.log(val.data.data);
+    });
+  }, [search]);
+
+  const onChange: PaginationProps['onChange'] = (page) => {
+    setCurrent(page);
+    const search2 = {...search, 
+      page: page}
+    searchTeacher(search2).then((val) => {
+      setDataSource(val.data.data);
+      setTotal(val.data.total);
+    });
+  };
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+    // Xử lý dữ liệu khi submit form
+    setSearch({
+      page: 1,
+      gender: gender,
+      name: name,
+      experience_year: experience,
+      fee: fee,
+    });
+  };
+
   return ( 
     <>
       <div className="container">
         <div className="flex gap-x-12">
-          <form className="my-14 w-1/3">
+          <form className="my-14 w-1/3" onSubmit={handleSubmit}>
           <div className="border-b border-gray-900/10 pb-12">
             <h2 className="text-xl font-bold leading-7 text-gray-900">フィルター</h2>
 
@@ -24,6 +84,8 @@ const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
                     name="name"
                     id="name"
                     autoComplete="given-name"
+                    value = {name}
+                    onChange={(e) => setName(e.target.value)}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -38,10 +100,15 @@ const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
                     id="gender"
                     name="gender"
                     autoComplete="gender"
+                    value = {gender}
+                    onChange={(e) => {
+                      e.target.value == ''? setGender(undefined):setGender(parseInt(e.target.value))}
+                    }
                     className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
                   >
-                    <option>男</option>
-                    <option>女</option>
+                    <option value=''>すべて</option>
+                    <option value='0'>男</option>
+                    <option value='1'>女</option>
                   </select>
                 </div>
               </div>
@@ -55,12 +122,17 @@ const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
                     id="fee"
                     name="fee"
                     autoComplete="fee"
+                    value = {fee}
+                    onChange={(e) => {
+                      e.target.value == ''? setFee(undefined):setFee(parseInt(e.target.value))}
+                    }
                     className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
                   >
-                    <option>25万ドン以下</option>
-                    <option>25-30万ドン</option>
-                    <option>30-35万ドン</option>
-                    <option>35万ドン以上</option>
+                    <option value="">すべて</option>
+                    <option value="1">25万ドン以下</option>
+                    <option value="2">25-30万ドン</option>
+                    <option value="3">30-35万ドン</option>
+                    <option value="4">35万ドン以上</option>
                   </select>
                 </div>
               </div>
@@ -74,27 +146,33 @@ const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
                     id="experience"
                     name="experience"
                     autoComplete="experience"
+                    value = {experience}
+                    onChange={(e) => {
+                      e.target.value == ''? setExperience(undefined):setExperience(parseInt(e.target.value))}
+                    }
                     className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
                   >
-                    <option>3年以下</option>
-                    <option>3-5年</option>
-                    <option>5-7年</option>
-                    <option>7年以上</option>
+                    <option value="">すべて</option>
+                    <option value="1">3年以下</option>
+                    <option value="2">3-5年</option>
+                    <option value="3">5-7年</option>
+                    <option value="4">7年以上</option>
                   </select>
                 </div>
               </div>
 
               <div className="sm:col-span-3">
-                <label htmlFor="experience" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="rate" className="block text-sm font-medium leading-6 text-gray-900">
                 評価点数
                 </label>
                 <div className="mt-2">
                   <select
-                    id="experience"
-                    name="experience"
-                    autoComplete="experience"
+                    id="rate"
+                    name="rate"
+                    autoComplete="rate"
                     className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6"
                   >
+                    <option value=""  disabled selected>--並べ替え--</option>
                     <option>最も高い</option>
                     <option>最も低い</option>
                   </select>
@@ -117,21 +195,41 @@ const SearchPage: React.FunctionComponent<SearchPageProps> = (props) => {
           </form>
 
           <section className="my-14 w-2/3">
-            <h2 className='mb-10 text-2xl font-medium'>検索結果</h2>
+            <p className='mb-2 text-xl font-semibold'>検索結果</p>
+            <p className='mb-5 text-sm font-extralight'>{`約${total}件の結果`}</p>
             <div className="grid grid-cols-2 gap-4">
-              {[...Array(4)].map(() => (
-                <div className="flex flex-col gap-3">
-                  <div className="h-60 w-full">
-                    <img
-                      src="https://iowacapitaldispatch.com/wp-content/uploads/2023/03/math-teacher-at-blackboard-1024x680.jpg"
-                      alt="Teacher"
-                      className="h-full w-full rounded-lg object-cover"
-                    />
+              {dataSource? 
+              dataSource.map((data) => (
+                  <div className="flex flex-col gap-3">
+                    <div className="h-60 w-full">
+                      <img
+                        src="https://iowacapitaldispatch.com/wp-content/uploads/2023/03/math-teacher-at-blackboard-1024x680.jpg"
+                        alt="Teacher"
+                        className="h-full w-full rounded-lg object-cover"
+                      />
+                    </div>
+                    <div className="font-bold text-xl">{data['name'] ? data['name'] : ""}</div>
+                    <div>
+                      <p className="font-semibold text-base leading-6 text-black-900">年齢 : 
+                      <span className="font-extralight pl-1 text-base leading-6 text-black-300"> {data['age']}</span></p>
+                    </div>
+                    <div>
+                      <p className="font-semibold text-base leading-6 text-black-900">授業料: 
+                      <span className="font-extralight pl-1 text-base leading-6 text-black-300"> {data['fee']} K</span></p>
+                    </div>
+                    <div>
+                      <p className="font-semibold text-base leading-6 text-black-900">経験年数 : 
+                      <span className="font-extralight pl-1 text-base leading-6 text-black-300"> {data['experience_year']}年</span></p>
+                    </div>
+                    <div>
+                      <p className="font-semibold text-base leading-6 text-black-900">詳細の情報 : 
+                      <span className="font-extralight pl-1 text-base leading-6 text-black-300"> {data['description']}</span></p>
+                    </div>
                   </div>
-                  <div className="font-bold text-xl">先生の名前</div>
-                  <div className="font-extralight leading-6 text-gray-600">先生の情報</div>
-                </div>
-              ))}
+              )): <></>}
+            </div>
+            <div className="flex mt-10 items-center justify-center">
+              <Pagination className="" current={current} onChange={onChange} total={total} />
             </div>
           </section>
         </div> 
